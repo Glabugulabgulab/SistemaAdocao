a9005ba578ee5126235144ddbdfde2d6641873b5
Glabugulabgulab <gui.gabi2010@hotmail.com>
2025-08-24 11:22:59 -0300

    Update sistema.c
    ignora a anterior

--- sistema.c
+++ sistema.c
@@ -19,18 +19,19 @@ int cadastrarAnimal(char E[10][20], char C[10][20], int ID[10], int Cod[10], int
     return num_animais + 1; //retorna total atualizado
 }
 
-void registrarAdocao(int Cod[10], int Status[10], int num_animais) {
+void registrarAdocao(char E[10][20], char C[10][20], int ID[10], int Cod[10], int Status[10], int num_animais) {
     printf("=== Registro de Adoção ===\n");
     int codigoBusca;
 
     printf("Informe o codigo do animal a ser adotado: ");
     scanf("%d", &codigoBusca); //pede um código de busca, pra comparar com todos os códigos no vetor Cod.
-
+    
     for (int i = 0; i < num_animais; i++) {
         if (Cod[i] == codigoBusca) {  //se encontrar o código equivalente, checa o status que está na mesma posição que o código (i).
             if (Status[i] == 0) {  // se o status estiver zerado ( não adotado) então muda pra um ( adotado)
                 Status[i] = 1;
                 printf("Adoção registrada com sucesso!\n\n");
+                printf("Espécie: %s | Idade: %d | Cor: %s | Código: %d\n\n", E[i], ID[i], C[i], Cod[i]);
             } else {   // se o status não for 0 ele passa pro else.
                 printf("Erro: Este animal ja foi adotado.\n\n");
             }
@@ -40,22 +41,50 @@ void registrarAdocao(int Cod[10], int Status[10], int num_animais) {
     printf("Erro: Animal com este codigo não foi encontrado.\n\n");
 }
 
-void pesquisarAnimal() {
+void pesquisarAnimal(char E[10][20], char C[10][20], int ID[10], int Cod[10], int num_animais) {
     printf("=== Pesquisa de Animal ===\n");
-    // Aqui vocÃª colocaria o cÃ³digo para pesquisar um animal
-    printf("Animal encontrado (exemplo)!\n\n");
+    int codigoBusca;
+    printf("Informe o código do animal: ");
+    scanf("%d", &codigoBusca);
+
+    for (int i = 0; i < num_animais; i++) {
+        if (Cod[i] == codigoBusca) {
+            printf("\nAnimal encontrado:\n");
+            printf("Espécie: %s | Idade: %d | Cor: %s | Código: %d\n\n", E[i], ID[i], C[i], Cod[i]);
+            return;
+        }
+    }
+    printf("Animal não encontrado!\n\n");
 }
 
-void relatorioAdocoes() {
-    printf("=== RelatÃ³rio de AdoÃ§Ãµes ===\n");
-    // Aqui vocÃª colocaria o cÃ³digo para imprimir relatÃ³rio de adoÃ§Ãµes
-    printf("Lista de adoÃ§Ãµes...\n\n");
+void relatorioAdocoes(char E[10][20], char C[10][20], int ID[10], int Cod[10], int Status[10], int num_animais) {
+    printf("=== Relatório de Adoções ===\n");
+    int encontrou = 0;
+    for (int i = 0; i < num_animais; i++) {
+        if (Status[i] == 1) {
+            printf("Espécie: %s | Idade: %d | Cor: %s | Código: %d\n", E[i], ID[i], C[i], Cod[i]);
+            encontrou = 1;
+        }
+    }
+    if (!encontrou) {
+        printf("Nenhum animal adotado.\n");
+    }
+    printf("\n");
 }
 
-void relatorioAnimaisDisponiveis() {
-    printf("=== RelatÃ³rio de Animais DisponÃ­veis ===\n");
-    // Aqui vocÃª colocaria o cÃ³digo para imprimir relatÃ³rio de animais disponÃ­veis
-    printf("Lista de animais disponÃ­veis...\n\n");
+void relatorioAnimaisDisponiveis(char E[10][20], char C[10][20], int ID[10], int Cod[10], int Status[10], int num_animais) {
+    printf("=== Relatório de Animais Disponíveis ===\n");
+    int encontrou = 0;
+    for (int i = 0; i < num_animais; i++) {
+        if (Status[i] == 0) {
+            printf("Espécie: %s | Idade: %d | Cor: %s | Código: %d\n", E[i], ID[i], C[i], Cod[i]);
+            encontrou = 1;
+        }
+    }
+    if (!encontrou) {
+        printf("Nenhum animal disponível.\n");
+    }
+    printf("\n");
 }
 
 int main() {
@@ -86,16 +115,16 @@ int main() {
                 num_animais = cadastrarAnimal(E, C, ID, Cod, Status, num_animais);
                 break;
             case 2:
-                registrarAdocao(Cod, Status, num_animais);
+                registrarAdocao(E, C, ID, Cod, Status, num_animais);
                 break;
             case 3:
-                pesquisarAnimal();
+                pesquisarAnimal(E, C, ID, Cod, num_animais);
                 break;
             case 4:
-                relatorioAdocoes();
+                relatorioAdocoes(E, C, ID, Cod, Status, num_animais);
                 break;
             case 5:
-                relatorioAnimaisDisponiveis();
+                relatorioAnimaisDisponiveis(E, C, ID, Cod, Status, num_animais);
                 break;
             case 6:
                 printf("Saindo...\n");
@@ -108,6 +137,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
